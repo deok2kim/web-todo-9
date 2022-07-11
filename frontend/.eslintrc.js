cc30dc02aec87d258a90c8dd20d17c183b6a720f
Jinny <khj9709@icloud.com>
2022-07-12 00:24:24 +0900

    chore: :gear: eslint, prettier 설정파일 추가 #2

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:prettier/recommended', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  plugins: ['simple-import-sort'],
+};
