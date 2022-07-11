628d1eacad6181feea0a351d5793f8671099a5d3
Jinny <khj9709@icloud.com>
2022-07-12 01:29:54 +0900

    feat: :sparkles: path alias, sass loader 추가 #1
    path alias: src-> @ 로 매핑하였음.
    sass loader을 통해서 scss 파일 해석가능.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,74 @@
-/**
- * TODO :: 설정하기
- */
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const htmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const path = require("path");
+
+const isProductionMode = process.env.NODE_ENV === "production";
 
 module.exports = {
-  mode: "development",
-  // entry: './src/app.js'
-  entry: path.resolve(__dirname, './src', 'app.js'),
-  output: { 
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js',
+  mode: isProductionMode ? "production" : "development",
+  entry: path.resolve(__dirname, "./src", "app.js"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[hash].js",
     clean: true,
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  corejs: {
+                    version: 3,
+                  },
+                  targets: {
+                    browsers: [">= 1%, not dead"],
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [
+          isProductionMode ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(png|jpe?g|gif|otf|ttf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
-    })
-  ]
-}
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  devServer: {
+    hot: true,
+  },
+  devtool: "eval-source-map",
+};
