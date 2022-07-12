99d6934a64a743545773b7b0802c171f880a9827
Jin <khj9709@icloud.com>
2022-07-12 15:33:57 +0900

    lintfix: :shirt: lint 적용 #4

---
+++ frontend/src/libs/Component.js
@@ -0,0 +1,16 @@
+class Component {
+  constructor($container, initialState) {
+    this.$container = $container;
+    this.state = initialState || {};
+  }
+
+  setState() {}
+
+  setEvent() {}
+
+  template() {}
+
+  render() {}
+}
+
+export default Component;
