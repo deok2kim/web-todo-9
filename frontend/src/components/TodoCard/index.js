37c8d6c0ade38bb273871f2fecff1c5c9eb52985
deok2kim <dukgi77@naver.com>
2022-07-12 16:42:12 +0900

    feat: :sparkles: 카드 컴포넌트 상태에 따른 스타일링 #4

--- frontend/src/components/TodoCard/index.js
+++ frontend/src/components/TodoCard/index.js
@@ -6,12 +6,53 @@ import Component from '@/libs/Component';
 class TodoCard extends Component {
   constructor($container, initialState) {
     super($container, initialState);
+
+    this.state = {
+      inputs: {
+        title: 'Title',
+        body: 'Body',
+        author: 'Author',
+      },
+      cardStatus: 'editable',
+    };
+
     this.render();
+  }
+
+  setState(nextState) {
+    this.state = nextState;
+    this.render();
+  }
+
+  getCardStyleByStatus() {
+    const cardClassList = ['card'];
+    const { cardStatus } = this.state;
+    switch (cardStatus) {
+      case 'editable':
+        cardClassList.push('active');
+        break;
+
+      case 'removable':
+        cardClassList.push('removable');
+        break;
+
+      case 'floating':
+        cardClassList.push('floating');
+        break;
+
+      case 'remain':
+        cardClassList.push('remain');
+        break;
+
+      default:
+        break;
+    }
+    return cardClassList.join(' ');
   }
 
   template() {
     return `
-			<div class="card">
+			<div class="${this.getCardStyleByStatus()}">
 				<div class="card__wrapper">
 					<p class="card__title">TITLE</p>	
 					<p class="card__body">BODY</p>
