0db9e06316da8c730f31467e2164c744a859a324
Jin <khj9709@icloud.com>
2022-07-12 17:15:18 +0900

    feat: :sparkles: 카드상태에 따른 버튼 토글 #4

--- frontend/src/components/TodoCard/index.js
+++ frontend/src/components/TodoCard/index.js
@@ -13,7 +13,7 @@ class TodoCard extends Component {
         body: 'Body',
         author: 'Author',
       },
-      cardStatus: 'editable',
+      cardStatus: 'creatable',
     };
 
     this.render();
@@ -28,6 +28,7 @@ class TodoCard extends Component {
     const cardClassList = ['card'];
     const { cardStatus } = this.state;
     switch (cardStatus) {
+      case 'creatable':
       case 'editable':
         cardClassList.push('active');
         break;
@@ -50,6 +51,23 @@ class TodoCard extends Component {
     return cardClassList.join(' ');
   }
 
+  isCardActive() {
+    const { cardStatus } = this.state;
+    return cardStatus === 'editable' || cardStatus === 'creatable';
+  }
+
+  getTemplateForButton() {
+    const { cardStatus } = this.state;
+    return `
+			<div class="button-wrapper">
+				<button class="btn normal">취소</button>
+				<button class="btn accent">${cardStatus === 'creatable' ? '등록' : '수정'}</button>
+			</div>
+		`;
+  }
+
+  setButton() {}
+
   template() {
     return `
 			<div class="${this.getCardStyleByStatus()}">
@@ -57,11 +75,18 @@ class TodoCard extends Component {
 					<p class="card__title">TITLE</p>	
 					<p class="card__body">BODY</p>
 					<p class="card__author">author by WEB</p>
+					${this.isCardActive() ? this.getTemplateForButton() : ''}
 				</div>
-
-				<button class="card__btn-remove">
-					<img src=${remove} alt="remove-btn" />
-				</button>
+				${
+          this.isCardActive()
+            ? ''
+            : `
+								<button class="card__btn-remove">
+									<img src=${remove} alt="remove-btn" />
+								</button>
+							`
+        }
+				
 			</div>
 		`;
   }
