c309263a1eef5d2abeef0a3e2d844995235b7cc9
Jin <khj9709@icloud.com>
2022-07-13 21:03:28 +0900

    edit: :fire: cors 미들웨어 설치 #13

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import logger from "morgan";
 import baseRouter from "./routes/index.js";
+import cors from "cors";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use("/", baseRouter);
 
