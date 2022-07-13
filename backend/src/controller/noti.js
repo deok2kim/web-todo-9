57ce5d3f5873d39ada4a16f815dbd30b3526752f
deokgi kim <dukgi77@naver.com>
2022-07-13 21:59:09 +0900

    feat: :sparkles: Noti Read Api 생성 #15

--- backend/src/controller/noti.js
+++ backend/src/controller/noti.js
@@ -2,7 +2,25 @@ import connection from "../lib/db.js";
 import { createRes } from "../utils/create-response.js";
 import { statusCode } from "../utils/status-code.js";
 
-export const getNoti = async (req, res) => {};
+export const getNoti = async (req, res) => {
+  const LIMIT = 10;
+  try {
+    connection
+      .promise()
+      .query("select * from `Noti` limit ?", [LIMIT])
+      .then((notiResult) => {
+        const [rows] = notiResult;
+        res
+          .status(statusCode.OK)
+          .send(createRes.success(statusCode.OK, "success", rows));
+      });
+  } catch (e) {
+    console.error(e);
+    res
+      .status(statusCode.INTERNAL_ERROR)
+      .send(createRes.fail(statusCode.INTERNAL_ERROR, "서버 내부 오류"));
+  }
+};
 
 export const createNoti = async (req, res) => {
   try {
